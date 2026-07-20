446135efc5258cbd76f6a4784c0e7c90838e9fc1
Marcelo Frau <marcelofrau@gmail.com>
2026-07-20 20:55:35 +0200

    icons: add 44 Papirus file-type icons with extension mapping
    24x24 PNGs converted from Papirus SVG for image, video, audio, text,
    archive, and application types. EntryViewModel.ExtIcons maps 150+
    extensions to correct icons.

--- XFiles/Controls/ColumnListView.xaml.cs
+++ XFiles/Controls/ColumnListView.xaml.cs
@@ -28,13 +28,185 @@ namespace XFiles.Controls
         }
 
         public bool IsDrive { get; set; }
+        public string ArchiveRootPath { get; set; }
+
+        private static readonly Dictionary<string, string> ExtIcons = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            // Images
+            [".png"]  = "filetype-image-png-24.png",
+            [".jpg"]  = "filetype-image-jpeg-24.png",
+            [".jpeg"] = "filetype-image-jpeg-24.png",
+            [".gif"]  = "filetype-image-gif-24.png",
+            [".bmp"]  = "filetype-image-bmp-24.png",
+            [".svg"]  = "filetype-image-svg-24.png",
+            [".tiff"] = "filetype-image-tiff-24.png",
+            [".tif"]  = "filetype-image-tiff-24.png",
+            [".pbm"]  = "filetype-image-pbm-24.png",
+            [".pgm"]  = "filetype-image-pbm-24.png",
+            [".ppm"]  = "filetype-image-pbm-24.png",
+            [".tga"]  = "filetype-image-tga-24.png",
+            [".ico"]  = "filetype-image-png-24.png",
+            [".webp"] = "filetype-image-png-24.png",
+            [".heic"] = "filetype-image-jpeg-24.png",
+            [".heif"] = "filetype-image-jpeg-24.png",
+            [".raw"]  = "filetype-image-jpeg-24.png",
+            [".cr2"]  = "filetype-image-jpeg-24.png",
+            [".nef"]  = "filetype-image-jpeg-24.png",
+            [".arw"]  = "filetype-image-jpeg-24.png",
+
+            // Video
+            [".mp4"]  = "filetype-video-mp4-24.png",
+            [".avi"]  = "filetype-video-avi-24.png",
+            [".mkv"]  = "filetype-video-mkv-24.png",
+            [".webm"] = "filetype-video-webm-24.png",
+            [".flv"]  = "filetype-video-flv-24.png",
+            [".wmv"]  = "filetype-video-wmv-24.png",
+            [".mov"]  = "filetype-video-mp4-24.png",
+            [".mpg"]  = "filetype-video-mp4-24.png",
+            [".mpeg"] = "filetype-video-mp4-24.png",
+            [".m4v"]  = "filetype-video-mp4-24.png",
+            [".ts"]   = "filetype-video-mp4-24.png",
+            [".vob"]  = "filetype-video-mp4-24.png",
+            [".3gp"]  = "filetype-video-mp4-24.png",
+
+            // Audio
+            [".mp3"]  = "filetype-audio-mp3-24.png",
+            [".flac"] = "filetype-audio-flac-24.png",
+            [".wav"]  = "filetype-audio-wav-24.png",
+            [".ogg"]  = "filetype-audio-ogg-24.png",
+            [".m4a"]  = "filetype-audio-m4a-24.png",
+            [".wma"]  = "filetype-audio-generic-24.png",
+            [".aac"]  = "filetype-audio-generic-24.png",
+            [".opus"] = "filetype-audio-ogg-24.png",
+            [".mid"]  = "filetype-audio-generic-24.png",
+            [".midi"] = "filetype-audio-generic-24.png",
+
+            // Archives
+            [".zip"]  = "file-archive-24.png",
+            [".7z"]   = "file-archive-24.png",
+            [".rar"]  = "file-archive-24.png",
+            [".tar"]  = "filetype-application-tar-24.png",
+            [".gz"]   = "filetype-application-gzip-24.png",
+            [".bz2"]  = "filetype-application-tar-24.png",
+            [".xz"]   = "filetype-application-tar-24.png",
+            [".tgz"]  = "filetype-application-tar-24.png",
+            [".zst"]  = "filetype-application-tar-24.png",
+
+            // ISO / Disc
+            [".iso"]  = "filetype-application-iso-24.png",
+            [".img"]  = "filetype-application-iso-24.png",
+            [".cue"]  = "filetype-application-iso-24.png",
+            [".nrg"]  = "filetype-application-iso-24.png",
+            [".mdf"]  = "filetype-application-iso-24.png",
+
+            // Documents
+            [".pdf"]  = "filetype-application-pdf-24.png",
+            [".doc"]  = "filetype-text-generic-24.png",
+            [".docx"] = "filetype-text-generic-24.png",
+            [".xls"]  = "filetype-text-generic-24.png",
+            [".xlsx"] = "filetype-text-generic-24.png",
+            [".ppt"]  = "filetype-text-generic-24.png",
+            [".pptx"] = "filetype-text-generic-24.png",
+            [".odt"]  = "filetype-text-generic-24.png",
+            [".ods"]  = "filetype-text-generic-24.png",
+            [".rtf"]  = "filetype-text-generic-24.png",
+
+            // Text
+            [".txt"]  = "filetype-text-generic-24.png",
+            [".log"]  = "filetype-text-log-24.png",
+            [".out"]  = "filetype-text-log-24.png",
+            [".err"]  = "filetype-text-log-24.png",
+            [".md"]   = "filetype-text-markdown-24.png",
+            [".rst"]  = "filetype-text-markdown-24.png",
+            [".csv"]  = "filetype-text-generic-24.png",
+            [".tsv"]  = "filetype-text-generic-24.png",
+            [".nfo"]  = "filetype-text-generic-24.png",
+            [".ini"]  = "filetype-text-generic-24.png",
+            [".cfg"]  = "filetype-text-generic-24.png",
+            [".conf"] = "filetype-text-generic-24.png",
+            [".yaml"] = "filetype-text-generic-24.png",
+            [".yml"]  = "filetype-text-generic-24.png",
+            [".toml"] = "filetype-text-generic-24.png",
+            [".env"]  = "filetype-text-log-24.png",
+            [".srt"]  = "filetype-text-generic-24.png",
+            [".sub"]  = "filetype-text-generic-24.png",
+            [".ass"]  = "filetype-text-generic-24.png",
+            [".sql"]  = "filetype-text-generic-24.png",
+            [".tex"]  = "filetype-text-generic-24.png",
+            [".bib"]  = "filetype-text-generic-24.png",
+
+            // Code
+            [".py"]   = "filetype-text-python-24.png",
+            [".js"]   = "filetype-text-javascript-24.png",
+            [".jsx"]  = "filetype-text-javascript-24.png",
+            [".ts"]   = "filetype-text-javascript-24.png",
+            [".tsx"]  = "filetype-text-javascript-24.png",
+            [".c"]    = "filetype-text-c-24.png",
+            [".h"]    = "filetype-text-c-24.png",
+            [".cpp"]  = "filetype-text-cpp-24.png",
+            [".hpp"]  = "filetype-text-cpp-24.png",
+            [".cs"]   = "filetype-text-csharp-24.png",
+            [".java"] = "filetype-text-java-24.png",
+            [".rs"]   = "filetype-text-rust-24.png",
+            [".go"]   = "filetype-text-go-24.png",
+            [".rb"]   = "filetype-text-ruby-24.png",
+            [".pl"]   = "filetype-text-perl-24.png",
+            [".pm"]   = "filetype-text-perl-24.png",
+            [".lua"]  = "filetype-text-lua-24.png",
+            [".sh"]   = "filetype-text-shell-24.png",
+            [".bash"] = "filetype-text-shell-24.png",
+            [".zsh"]  = "filetype-text-shell-24.png",
+            [".ps1"]  = "filetype-text-shell-24.png",
+            [".bat"]  = "filetype-text-shell-24.png",
+            [".cmd"]  = "filetype-text-shell-24.png",
+            [".css"]  = "filetype-text-css-24.png",
+            [".scss"] = "filetype-text-css-24.png",
+            [".less"] = "filetype-text-css-24.png",
+            [".html"] = "filetype-text-css-24.png",
+            [".htm"]  = "filetype-text-css-24.png",
+            [".xml"]  = "filetype-text-xml-24.png",
+            [".json"] = "filetype-text-javascript-24.png",
+            [".vue"]  = "filetype-text-javascript-24.png",
+            [".swift"] = "filetype-text-csharp-24.png",
+            [".kt"]   = "filetype-text-java-24.png",
+            [".kts"]  = "filetype-text-java-24.png",
+            [".dart"] = "filetype-text-java-24.png",
+            [".ex"]   = "filetype-text-ruby-24.png",
+            [".exs"]  = "filetype-text-ruby-24.png",
+            [".hs"]   = "filetype-text-ruby-24.png",
+            [".ml"]   = "filetype-text-ruby-24.png",
+            [".zig"]  = "filetype-text-rust-24.png",
+            [".nim"]  = "filetype-text-rust-24.png",
+            [".v"]    = "filetype-text-c-24.png",
+            [".vh"]   = "filetype-text-c-24.png",
+            [".asm"]  = "filetype-text-c-24.png",
+            [".s"]    = "filetype-text-c-24.png",
+
+            // Executables
+            [".exe"]  = "filetype-application-executable-24.png",
+            [".msi"]  = "filetype-application-executable-24.png",
+            [".appx"] = "filetype-application-executable-24.png",
+            [".dll"]  = "filetype-application-executable-24.png",
+            [".so"]   = "filetype-application-executable-24.png",
+            [".dylib"] = "filetype-application-executable-24.png",
+        };
+
+        private static readonly string GenericIcon = "file-generic-24.png";
 
         public string Icon => IsDrive
             ? "ms-appx:///Assets/FileTypes/drive-harddisk-24.png"
             : IsDirectory
                 ? $"ms-appx:///Assets/FileTypes/folder-{_folderColor}-24.png"
                 : (IsArchive ? "ms-appx:///Assets/FileTypes/file-archive-24.png"
-                              : "ms-appx:///Assets/FileTypes/file-generic-24.png");
+                              : $"ms-appx:///Assets/FileTypes/{GetFileIcon(Name)}");
+
+        private static string GetFileIcon(string fileName)
+        {
+            var ext = System.IO.Path.GetExtension(fileName);
+            if (!string.IsNullOrEmpty(ext) && ExtIcons.TryGetValue(ext, out var icon))
+                return icon;
+            return GenericIcon;
+        }
 
         public string SizeDisplay => IsDirectory ? "" : FormatSize(SizeBytes);
 
@@ -192,6 +364,8 @@ namespace XFiles.Controls
 
         // --- INavigable ---
 
+        public bool IsMediaFullscreen => false;
+
         public void OnDPadUp()
         {
             if (EntryList.SelectedIndex > 0)
@@ -243,6 +417,13 @@ namespace XFiles.Controls
             Log.Verbose("ColumnListView.OnPageDown: index={Index}", EntryList.SelectedIndex);
         }
 
+        public void OnSeekBack() { }
+        public void OnSeekForward() { }
+        public void OnSeekRepeat(int seconds) { }
+        public void OnTriggerHeld(float leftTrigger, float rightTrigger) { }
+        public void OnLeftStickMove(float x, float y) { }
+        public void OnRightStickMove(float x, float y) { }
+
         public void OnScrollHorizontal(double delta) { }
 
         public void OnScrollVertical(double delta) { }
@@ -258,6 +439,17 @@ namespace XFiles.Controls
         {
             switch (e.Key)
             {
+                case Windows.System.VirtualKey.Up:
+                case Windows.System.VirtualKey.Down:
+                case Windows.System.VirtualKey.Left:
+                case Windows.System.VirtualKey.Right:
+                case Windows.System.VirtualKey.Enter:
+                case Windows.System.VirtualKey.GamepadA:
+                case Windows.System.VirtualKey.GamepadB:
+                case Windows.System.VirtualKey.GamepadDPadUp:
+                case Windows.System.VirtualKey.GamepadDPadDown:
+                case Windows.System.VirtualKey.GamepadDPadLeft:
+                case Windows.System.VirtualKey.GamepadDPadRight:
                 case Windows.System.VirtualKey.PageUp:
                 case Windows.System.VirtualKey.PageDown:
                 case Windows.System.VirtualKey.GamepadLeftTrigger:
